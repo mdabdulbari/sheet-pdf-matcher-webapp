7980c7ede13347abf419e2f1f626ca34eca56645
Mohammed Farhan Hussain <farhanleo007@gmail.com>
2025-01-21 21:01:27 +0530

    fix overflow X for table

--- app/page.jsx
+++ app/page.jsx
@@ -6,53 +6,53 @@ import PdfViewer from "./components/PdfViewer";
 import Header from "./components/Header";
 
 const FileUploadPage = () => {
-	const [rows, setRows] = useState([]);
-	const [columns, setColumns] = useState([]);
-	const [pdfFile, setPdfFile] = useState(null);
+  const [rows, setRows] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const [pdfFile, setPdfFile] = useState(null);
 
-	return (
-		<Box
-			sx={{
-				width: "100%",
-				height: "100%",
-				display: "flex",
-				flexDirection: "column",
-			}}
-		>
-			<Header title="Ledger & Statement Matcher" />
-			<Grid container spacing={2} sx={{ padding: 2, flexGrow: 1 }}>
-				<Grid size={6}>
-					<Paper
-						variant="outlined"
-						sx={{
-							height: "100%",
-							padding: 2,
-							boxSizing: "border-box",
-						}}
-					>
-						<SpreadsheetViewer
-							rows={rows}
-							setRows={setRows}
-							columns={columns}
-							setColumns={setColumns}
-						/>
-					</Paper>
-				</Grid>
-				<Grid size={6}>
-					<Paper
-						variant="outlined"
-						sx={{
-							height: "100%",
-							padding: 2,
-							boxSizing: "border-box",
-						}}
-					>
-						<PdfViewer pdfFile={pdfFile} setPdfFile={setPdfFile} />
-					</Paper>
-				</Grid>
-			</Grid>
-		</Box>
-	);
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Header title="Ledger & Statement Matcher" />
+      <Grid container spacing={2} sx={{  padding: 2, flexGrow: 1 }}>
+        <Grid sx={{ overflowX: "hidden" }} size={6}>
+          <Paper
+            variant="outlined"
+            sx={{
+              height: "100%",
+              padding: 2,
+              boxSizing: "border-box",
+            }}
+          >
+            <SpreadsheetViewer
+              rows={rows}
+              setRows={setRows}
+              columns={columns}
+              setColumns={setColumns}
+            />
+          </Paper>
+        </Grid>
+        <Grid size={6}>
+          <Paper
+            variant="outlined"
+            sx={{
+              height: "100%",
+              padding: 2,
+              boxSizing: "border-box",
+            }}
+          >
+            <PdfViewer pdfFile={pdfFile} setPdfFile={setPdfFile} />
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default FileUploadPage;
